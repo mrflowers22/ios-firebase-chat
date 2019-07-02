b2584cf0855687576509544768bff7d7a9e8808f
Michael R.F <mrflowers22@uwalumni.com>
2019-07-02 12:28:34 -0700

    constructed the storyboard

---
+++ FirebaseChatHW/FirebaseChatHW/Model/ChatRoom.swift
@@ -0,0 +1,18 @@
+//
+//  ChatRoom.swift
+//  FirebaseChatHW
+//
+//  Created by Michael Flowers on 7/2/19.
+//  Copyright © 2019 Michael Flowers. All rights reserved.
+//
+
+import Foundation
+import Firebase
+import MessageKit
+
+struct ChatRoom {
+    var room: String
+    var messages: [Message]
+}
+
+
